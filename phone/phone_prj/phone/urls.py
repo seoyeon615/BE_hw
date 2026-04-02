3bffdee57689f56df9caac6d7e45c9794c8274c0
seoyeon615 <bsy070615@duksung.ac.kr>
2026-04-02 17:06:35 +0900

    [0402] 스터디 과제

--- phone/phone_prj/phone/urls.py
+++ phone/phone_prj/phone/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import *
+from .views import result, create, delete, detail, update, phone_list
+
 
 app_name = 'phone'
 
 urlpatterns = [
-    path('', list, name='list'),
+    path('', phone_list, name='phone_list'),
     path('result/', result, name='result'),
+    path('create/', create, name='create'),
+    path('delete/<int:id>', delete, name='delete'),
+    path('detail/<int:id>', detail, name='detail'),
+    path('update/<int:id>', update, name='update'),
 ]
--- phone/phone_prj/phone/views.py
+++ phone/phone_prj/phone/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 
-def list(request):
+def phone_list(request):
     posts = Post.objects.all().order_by('name')
-    return render(request, 'phone/list.html', {'posts':posts})
+    return render(request, 'phone/phone_list.html', {'posts':posts})
 
 def result(request):
     query = request.GET.get('name')
@@ -15,3 +15,43 @@ def result(request):
 
     return render(request, 'phone/result.html', {'posts': posts,'query': query
     })   
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        number = request.POST.get('number')
+        email = request.POST.get('email')
+
+        Post.objects.create(
+            name=name,
+            number=number,
+            email=email
+        )
+        return redirect('phone:phone_list')
+
+    return render(request, 'phone/create.html')
+
+def delete(request, id):
+    phone = Post.objects.get(id=id)
+    if request.method == 'POST':
+        phone.delete()
+        return redirect('phone:phone_list')
+    return render(request, 'phone/delete.html', {'phone': phone})
+
+def detail(request, id):
+    post = get_object_or_404(Post, id=id)
+
+
+    return render(request, 'phone/detail.html', {'post':post})
+
+def update(request, id):
+    phone = Post.objects.get(id=id)
+
+    if request.method == 'POST':
+        phone.name = request.POST.get('name')
+        phone.number = request.POST.get('number')
+        phone.email = request.POST.get('email')
+        phone.save()
+        return redirect('phone:detail', phone.id)
+
+    return render(request, 'phone/update.html', {'phone': phone})
